eb695ce51ff2bf294870970d12fff532a2a67f1c
KingOfOdonata <tschneider.dreamcreative@gmail.com>
2021-07-29 22:31:32 -0400

    Game Unboxing Title

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,59 @@
-document.addEventListener("DOMContentLoaded", function() {
-  fields.firstName = document.getElementById('firstName');
-  fields.lastName = document.getElementById('lastName');
-  fields.email = document.getElementById('email');
-  fields.houseNumber = document.getElementById('houseNumber');
-  fields.events = document.getElementById('events');
-  fields.question = document.getElementById('question');
- })
-
- function isNotEmpty(value) {
-  if (value == null || typeof value == 'undefined' ) return false;
-  return (value.length > 0);
- }
-
- function isEmail(email) {
-  let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-  return regex.test(String(email).toLowerCase());
- }
-
- function fieldValidation(field, validationFunction) {
-  if (field == null) return false;
- 
-  let isFieldValid = validationFunction(field.value)
-  if (!isFieldValid) {
-  field.className = 'placeholderRed';
-  } else {
-  field.className = '';
-  }
- 
-  return isFieldValid;
- }
-
- function isValid() {
-  var valid = true;
-  
-  valid &= fieldValidation(fields.firstName, isNotEmpty);
-  valid &= fieldValidation(fields.lastName, isNotEmpty);
-  valid &= fieldValidation(fields.email, isEmail);
-  valid &= fieldValidation(fields.question, isNotEmpty);
- 
-  return valid;
- }
-
- class User {
-  constructor(firstName, lastName, email, events, question) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.email = email;
-  this.events = events;
-  this.question = question;
-  }
- }
-
- function sendContact () {
-
-if (isValid()) {
-  let usr = new User (firstName.value, lastName.value, email.value, newsletter.checked);
-
-  alert(`${usr.firstName} Thanks for contacting us!.`)
-} else {
-  alert("There was an error. Please check you completed information.")
+// show a message with a type of the input
+function showMessage(input, message, type) {
+	// get the small element and set the message
+	const msg = input.parentNode.querySelector("small");
+	msg.innerText = message;
+	// update the class for the input
+	input.className = type ? "success" : "error";
+	return type;
 }
 
- }
- 
+function showError(input, message) {
+	return showMessage(input, message, false);
+}
 
+function showSuccess(input) {
+	return showMessage(input, "", true);
+}
+
+function hasValue(input, message) {
+	if (input.value.trim() === "") {
+		return showError(input, message);
+	}
+	return showSuccess(input);
+}
+
+function validateEmail(input, requiredMsg, invalidMsg) {
+	// check if the value is not empty
+	if (!hasValue(input, requiredMsg)) {
+		return false;
+	}
+	// validate email format
+	const emailRegex =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	const email = input.value.trim();
+	if (!emailRegex.test(email)) {
+		return showError(input, invalidMsg);
+	}
+	return true;
+}
+
+const form = document.querySelector("#signup");
+
+const NAME_REQUIRED = "Please enter your name";
+const EMAIL_REQUIRED = "Please enter your email";
+const EMAIL_INVALID = "Please enter a correct email address format";
+
+form.addEventListener("submit", function (event) {
+	// stop form submission
+	event.preventDefault();
+
+	// validate the form
+	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
+	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+	// if valid, submit the form.
+	if (nameValid && emailValid) {
+		alert("Demo only. No form was posted.");
+	}
+});
