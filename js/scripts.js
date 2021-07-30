eedb382a05c08f0d6abd5f3a43ecc4f066c8db30
KingOfOdonata <tschneider.dreamcreative@gmail.com>
2021-07-30 00:45:22 -0400

    Contact Form and Javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,38 @@
-// show a message with a type of the input
-function showMessage(input, message, type) {
-	// get the small element and set the message
-	const msg = input.parentNode.querySelector("small");
-	msg.innerText = message;
-	// update the class for the input
-	input.className = type ? "success" : "error";
-	return type;
-}
+const submitBtn = document.getElementById("submit-btn");
 
-function showError(input, message) {
-	return showMessage(input, message, false);
-}
+const validate = (e) => {
+  e.preventDefault();
+  const name = document.getElementById("name");
+  const emailAddress = document.getElementById("email-address");
+  const comments = document.getElementById("comments");
+  if (name.value === "") {
+    alert("Please enter your name.");
+    name.focus();
+    return false;
+  }
 
-function showSuccess(input) {
-	return showMessage(input, "", true);
-}
+  if (emailAddress.value === "") {
+    alert("Please enter your email address.");
+    emailAddress.focus();
+    return false;
+  }
 
-function hasValue(input, message) {
-	if (input.value.trim() === "") {
-		return showError(input, message);
-	}
-	return showSuccess(input);
-}
+  if (!emailIsValid(emailAddress.value)) {
+    alert("Please enter a valid email address.");
+    emailAddress.focus();
+    return false;
+  }
 
-function validateEmail(input, requiredMsg, invalidMsg) {
-	// check if the value is not empty
-	if (!hasValue(input, requiredMsg)) {
-		return false;
-	}
-	// validate email format
-	const emailRegex =
-		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (comments.value === "") {
+    alert("Please enter your question or comment");
+    comments.focus();
+    return false;
+  }
+  return true; // Can submit the form data to the server
+};
 
-	const email = input.value.trim();
-	if (!emailRegex.test(email)) {
-		return showError(input, invalidMsg);
-	}
-	return true;
-}
+const emailIsValid = (email) => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
 
-const form = document.querySelector("#signup");
-
-const NAME_REQUIRED = "Please enter your name";
-const EMAIL_REQUIRED = "Please enter your email";
-const EMAIL_INVALID = "Please enter a correct email address format";
-
-form.addEventListener("submit", function (event) {
-	// stop form submission
-	event.preventDefault();
-
-	// validate the form
-	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
-	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
-	// if valid, submit the form.
-	if (nameValid && emailValid) {
-		alert("Demo only. No form was posted.");
-	}
-});
+submitBtn.addEventListener("click", validate);
